f898a56af6cce3d471cbf8658305b9d35ab7acbe
duonghan <20155296@student.hust.edu.vn>
2017-03-12 01:33:51 +0700

    Fix bug lan 1

--- BT tren lop/tochuclaidong.cpp
+++ BT tren lop/tochuclaidong.cpp
@@ -5,59 +5,70 @@
 
 using namespace std;
 
-const int MAX = 1000;
-int count,i,j;
+const int MAX = 1024;
+int count,i;//count: kich thuoc day so int
 
-void creatSomething(int arr[], int top, int bot, int &mark){
-	if(bot-top<2){
+//gop cac so lien tuc va tinh so ki tu da in ra man hinh
+void output(int arr[], int top, int bot, int &mark){
+	//neu cho ca 2 so lien tuc nhau thi xuat ra, dong thoi tang them 3 ki tu cho moi so
+	if(bot-top < 2){
 		for(int k = top; k<= bot; k++)
 		{
 			mark+=3;
-			if(mark > 30){
-			cout<<endl;
-			mark=3;
+			if(mark > 30){//xuong dong va thiet dat lai mark
+				cout<<endl;
+				mark=3;//chuyen so xuong hang duoi
 			}
-			cout<<arr[top]<<",";
+			cout<<arr[k]<<",";
 		}
-	}else{
+	}
+	else{//sau khi gop thi tang them 7 ki tu(them dau '-')
 		mark+=7;
 		if(mark > 30){
 			cout<<endl;
-			mark=3;
+			mark=7;
 		}
 		cout<<arr[top]<<"-"<<arr[bot]<<",";
-		
 	}
+
 	
-	
-}
-void display(int * arr, int length){
-	if(length ==0 ) return;
-	int top=0,mark =0;
-	
-	for(int i=0; i< length; i++){
-		
-	if(arr[i] != arr[top] + i - top){
-		creatSomething(arr,top,i-1,mark);
-		top=i;
-	}
-	if(arr[i] == arr[top] + i - top) continue;
-	}
-	cout<<endl<<mark;
 }
 
-int chuyen(char c){
+//hien thi day so ra man hinh
+void display(int * arr, int length){
+	if(length ==0 ) return;
+
+	int top=0,mark =0;//danh dau chi so phan tu dau va so ki tu da duyet xong
+	
+	for(i=0; i< length; i++){
+	if (i==length -1)
+		{
+			output(arr,top,i,mark);
+			return;
+		}
+	
+		if(arr[i] != arr[top] + i - top){
+			output(arr,top,i-1,mark);
+			top=i;
+		}
+		if(arr[i] == arr[top] + i - top) continue;
+	}
+	
+	
+}
+
+int changeType(char c){//chuyen ki tu sang so tuong ung
 	return (int)c -48;
 }
 
-int * strtoDigit(char * str, int length){
-	j=0;
+int * strtoDigit(char * str, int length){//chuyen doi day so dang string sang int
+	int j=0;
 	int * digit = new int[length];
 	
 	for(i=0; i< length; i++){
 		if(str[i] == ','|| str[i]== ' ') continue;
 		else{
-			digit[j] = chuyen(str[i])*100 + chuyen(str[i+1])*10 + chuyen(str[i+2]);
+			digit[j] = changeType(str[i])*100 + changeType(str[i+1])*10 + changeType(str[i+2]);
 			j++;
 			i+=2;
 		}
@@ -70,10 +81,16 @@ int main(){
 	char * str = new char[MAX];
 	cout<<"Nhap vao day so: ";
 	cin>>str;
+
 	int length = strlen(str);
-	int * digit = new int[count];
+	int * digit = new int[MAX];
 	digit = strtoDigit(str, length);
+
+	cout<<endl<<"------------------------------"<<endl;
+	cout<<"Day so khi in tren nhieu dong:"<<endl;
 	display(digit,count);
+
+	//giai phong bo nho
 	delete[] str;
 	delete[] digit;
 	return 0;
